761280e3c32b4c1939b797688f3a7ed6cd4e4cb7
Dan Tucker <furymuso@aim.com>
2017-10-03 09:57:46 +0100

    added actual cms forms page

---
+++ cms-form.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+<form action="PDO-insert.php" method="POST">
+    <h4>Add to About Me</h4>
+    <p>Add Section<input type="text" name="content_name"></p>
+    <input type="text" name="text_content"></p>
+    <input type="submit" value="Submit">
+</form>
+<form action="PDO-insert.php" method="POST">
+    <h4>Edit About Me</h4>
+    <p>Edit Section Name<input type="text" name="content_name"></p>
+    <p>Edit Text<input type="text" name="text_content"></p>
+    <input type="submit" value="Submit">
+</form>
+    <input type="submit" value="Submit">
+</form>
+</body>
+</html>
