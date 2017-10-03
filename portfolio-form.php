c4bf431f40debc186ab0177eb9d37011f5b61255
Dan Tucker <furymuso@aim.com>
2017-10-03 11:24:35 +0100

    completed basic portfolio cms form

--- portfolio-form.php
+++ portfolio-form.php
@@ -4,12 +4,12 @@
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,800" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Raleway: 400,700" rel="stylesheet">
     <link href="cms-form.css" type="text/css" rel="stylesheet">
-    <title>CMS page</title>
+    <title>Portfolio CMS page</title>
 </head>
 <body>
 <div class="cmsForm">
     <form action="PDO-insert.php" method="POST">
-        <h1>Add to About Me</h1>
+        <h1>Add to Portfolio</h1>
         <h2>Add Section</h2>
             <input type="text" name="content_name">
         <h2>Add Text</h2>
@@ -18,7 +18,7 @@
             <input class="editingButton" type="submit" value="Add">
     </form>
     <form action="PDO-insert.php" method="POST">
-        <h1>Edit About Me</h1>
+        <h1>Edit Portfolio</h1>
         <h2>Edit Section Name</h2>
             <input type="text" name="content_name">
         <h2>Edit Text</h2>
@@ -27,7 +27,7 @@
             <input class="editingButton" type="submit" value="Make Changes">
     </form>
     <form action="" method="POST">
-        <h1>Delete About Me</h1>
+        <h1>Delete From Portfolio</h1>
         <br>
             <input class="editingButton" type="submit" value="Delete">
     </form>
