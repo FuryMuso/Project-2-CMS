ca9a1dd583a811338710b04378b86760e3964257
Dan Tucker <furymuso@aim.com>
2017-10-04 15:41:48 +0100

    added insert query to insert edited data into database

--- home-edit-insert-pdo.php
+++ home-edit-insert-pdo.php
@@ -8,8 +8,9 @@ function editHomeContent()
     $db = new PDO('mysql:host=127.0.0.1;dbname=CMSproject', 'root');
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-    $query = $db->prepare("SELECT `section_title`, FROM `Home` WHERE ;");
+    $query = $db->prepare("INSERT INTO `About` (`section_title`, `text_input`) VALUES (:section_title, :text_input);");
     $query->bindParam(':section_title', $_POST['toEdit']);
+    $query->bindParam(':text_input', $_POST['toEdit']);
     $query->execute();
 }
 
