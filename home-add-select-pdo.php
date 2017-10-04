58155c2a264b10eab8f996d0250c19008bd26c46
Dan Tucker <furymuso@aim.com>
2017-10-04 11:00:34 +0100

    added doc block to select pdo of add to home

--- home-add-select-pdo.php
+++ home-add-select-pdo.php
@@ -1,18 +1,21 @@
 <?php
 
+/**
+ * select data from database
+ *
+ * @return array
+ */
+function retrieveData()
+{
+    $db = new PDO('mysql:host=127.0.0.1;dbname=CMSproject', 'root');
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-$db = new PDO('mysql:host=127.0.0.1;dbname=CMSproject', 'root');
-$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    $query = $db->prepare("SELECT `section_title`,`text_input` FROM `Home`;");
+    $query->execute();
 
-$query = $db->prepare("SELECT `section_title`,`text_input` FROM `Home`;");
-$query->execute();
+    return $query->fetchAll();
 
-$result = $query->fetchAll();
 
-var_dump($result);
-
-echo "<br>";
-
-//$var = $result[1]['content_name'];
+}
 
 ?>
