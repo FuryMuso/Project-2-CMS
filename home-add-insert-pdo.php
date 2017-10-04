7b8c1954e0f9b22904ec52d70e5678fda9e6adba
Dan Tucker <furymuso@aim.com>
2017-10-04 10:55:52 +0100

    added doc block to insert pdo of home form

--- home-add-insert-pdo.php
+++ home-add-insert-pdo.php
@@ -1,11 +1,25 @@
 <?php
 
-$db = new PDO('mysql:host=127.0.0.1;dbname=CMSproject', 'root');
-$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+/**
+ *function created to insert data into database
+ *
+ * @param
+ * @param
+ *
+ * return data is sent to and displayed in the database
+ */
+function addHomeContent()
+{
+    $db = new PDO('mysql:host=127.0.0.1;dbname=CMSproject', 'root');
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-$query = $db->prepare("INSERT INTO `Home` (`section_title`, `text_input`) VALUES (:section_title, :text_input);");
-$query->bindParam(':section_title', $_POST['section_title']);
-$query->bindParam(':text_input', $_POST['text_input']);
-$query->execute();
+    $query = $db->prepare("INSERT INTO `Home` (`section_title`, `text_input`) VALUES (:section_title, :text_input);");
+    $query->bindParam(':section_title', $_POST['section_title']);
+    $query->bindParam(':text_input', $_POST['text_input']);
+    $query->execute();
+}
 
+addHomeContent();
+
+header('Location: home-form.php');
 ?>
