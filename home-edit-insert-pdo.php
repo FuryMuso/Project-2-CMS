a49d1416344067e7870f669a1fea43c1cdb652cc
Dan Tucker <furymuso@aim.com>
2017-10-04 15:04:14 +0100

    amended a naming error when calling the function

--- home-edit-insert-pdo.php
+++ home-edit-insert-pdo.php
@@ -1,11 +1,18 @@
 <?php
 
-$db = new PDO('mysql:host=127.0.0.1;dbname=CMSproject', 'root');
-$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+/**
+ * function created to place data into About section of database
+ */
+function editHomeContent()
+{
+    $db = new PDO('mysql:host=127.0.0.1;dbname=CMSproject', 'root');
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-$query = $db->prepare("UPDATE `Home` SET `section_title` = :section_title, `text_input` = :text_input; WHERE `id` = ;");
-$query->bindParam(':section_title', $_POST['section_title']);
-$query->bindParam(':text_input', $_POST['text_input']);
-$query->execute();
+    $query = $db->prepare("SELECT `section_title`, FROM `Home` WHERE ;");
+    $query->bindParam(':section_title', $_POST['toEdit']);
+    $query->execute();
+}
+
+header ('Location: home-form.php');
 
 ?>
