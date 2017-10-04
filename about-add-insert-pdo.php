90832119a4d603ad4345e62082693455d1c33056
Dan Tucker <furymuso@aim.com>
2017-10-04 14:40:13 +0100

    function created and doc block added

--- about-add-insert-pdo.php
+++ about-add-insert-pdo.php
@@ -1,11 +1,18 @@
 <?php
 
-$db = new PDO('mysql:host=127.0.0.1;dbname=CMSproject', 'root');
-$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+function addAboutContent()
+{
+    $db = new PDO('mysql:host=127.0.0.1;dbname=CMSproject', 'root');
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-$query = $db->prepare("INSERT INTO `About` (`section_title`, `text_input`) VALUES (:section_title, :text_input);");
-$query->bindParam(':section_title', $_POST['section_title']);
-$query->bindParam(':text_input', $_POST['text_input']);
-$query->execute();
+    $query = $db->prepare("INSERT INTO `About` (`section_title`, `text_input`) VALUES (:section_title, :text_input);");
+    $query->bindParam(':section_title', $_POST['section_title']);
+    $query->bindParam(':text_input', $_POST['text_input']);
+    $query->execute();
+}
+
+addAboutContent();
+
+header('Location: home-form.php');
 
 ?>
