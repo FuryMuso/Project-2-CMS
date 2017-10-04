2e9717412604ed74e74e93b84322447204de2d97
Dan Tucker <furymuso@aim.com>
2017-10-04 15:24:03 +0100

    updated code to function and added doc block

--- portfolio-add-insert-pdo.php
+++ portfolio-add-insert-pdo.php
@@ -1,5 +1,8 @@
 <?php
 
+/**
+ * function to input data into Portfolio table in database
+ */
 function addPortfolioContent()
 {
     $db = new PDO('mysql:host=127.0.0.1;dbname=CMSproject', 'root');
