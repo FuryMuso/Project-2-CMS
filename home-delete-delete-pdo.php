3ef51bbb827b74a1ae64f8ed6ad2ea0d4a21ba57
Dan Tucker <furymuso@aim.com>
2017-10-04 14:43:13 +0100

    updated code to function and added doc block

--- home-delete-delete-pdo.php
+++ home-delete-delete-pdo.php
@@ -1,10 +1,20 @@
 <?php
 
-$db = new PDO('mysql:host=127.0.0.1;dbname=CMSproject', 'root');
-$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+/**
+ * function to delete content from Home table in database
+ */
+function deleteHomeContent()
+{
+    $db = new PDO('mysql:host=127.0.0.1;dbname=CMSproject', 'root');
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-$query = $db->prepare("UPDATE `Home` SET `deleted` = 1 WHERE `id` = :id;");
-$query->bindParam(':id', $_POST['id']);
-$query->execute();
+    $query = $db->prepare("UPDATE `Home` SET `deleted` = 1 WHERE `section_title` = :section_title;");
+    $query->bindParam(':section_title', $_POST['toDelete']);
+    $query->execute();
+}
+
+deleteHomeContent();
+
+header('Location: home-form.php');
 
 ?>
