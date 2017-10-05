3031fccf2070c2fd20e8255c5f55dd82e23b0cd4
Dan Tucker <furymuso@aim.com>
2017-10-05 16:21:51 +0100

    changed head  title

--- about-form.php
+++ about-form.php
@@ -1,11 +1,13 @@
 <?php
 
+include_once('about-add-select-pdo.php');
 include_once('loginProcess.php');
 
 if(!isLoggedIn()) {
     header('Location: login.php');
 }
 
+$aboutData = retrieveAboutData();
 ?>
 
 <!DOCTYPE html>
@@ -14,12 +16,12 @@ if(!isLoggedIn()) {
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,800" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Raleway: 400,700" rel="stylesheet">
     <link href="cms-form.css" type="text/css" rel="stylesheet">
-    <title>About CMS Form</title>
+    <title>About</title>
 </head>
 <body>
 <div class="cmsNav">
     <div class="navOption">
-        <a href="home-form.php"><input class="aboutOption" type="submit" value="Home"></a>
+        <a href="home-form.php"><input class="HomeOption" type="submit" value="Home"></a>
         <a href="portfolio-form.php"><input class="portfolioOption" type="submit" value="Portfolio"></a>
     </div>
 </div>
@@ -27,26 +29,54 @@ if(!isLoggedIn()) {
     <input type="submit" value="LogOooot">
 </form>
 <div class="cmsForm">
-    <form action="about-insert.php" method="POST">
+    <form action="about-add-insert-pdo.php" method="POST">
         <h1>Add to About Me</h1>
         <h2>Add Section</h2>
-            <input type="text" name="section_title">
+        <select name="section_title">
+            <?php
+            foreach ($aboutData as $addOption) {
+                echo "<option value='" . $addOption['id'] . "'>" . $addOption['id'] . ' - ' . $addOption['section_title'] . "</option>";
+            }
+            ?>
+        </select>
         <h2>Add Text</h2>
-            <textarea rows="16" name="text_input" name="" cols="75"></textarea>
-        <br>
-            <input class="editingButton" type="submit" value="Add">
-    </form>
-    <form action="PDO-insert.php" method="POST">
-        <h1>Edit About Me</h1>
-        <h2>Edit Section Name</h2>
-            <input type="text" name="section_title">
-        <h2>Edit Text</h2>
             <textarea rows="16" name="text_input" cols="75"></textarea>
         <br>
-            <input class="editingButton" type="submit" value="Make Changes">
+        <input class="editingButton" type="submit" value="Add">
     </form>
-    <form action="" method="POST">
-        <h1>Delete From About Me</h1>
+    <form action="about-form.php" method="POST">
+        <h1>Edit About</h1>
+        <h3>Edit Options</h3>
+        <select class="editAboutOptions" name="toEdit">
+            <?php
+            foreach ($aboutData as $editOption) {
+                echo "<option value='" . $editOption['id'] . "'>" . $editOption['id'] . ' - ' . $editOption['section_title'] . "</option>";
+            }
+            ?>
+        </select><br>
+        <input type="submit" value="Edit This Section">
+    </form>
+    <?php
+    if ($_POST['toEdit']) {
+        $currentSection = findSection($_POST['toEdit'], $aboutData);?>
+        <form action="about-edit-insert-pdo.php" method="POST">
+            <input type="hidden" value="<?php echo $currentSection['id']; ?>" name="id">
+            <h2>Edit Text</h2>
+            <textarea rows="16" name="text_input" cols="75"><?php echo $currentSection['text_input']; ?></textarea>
+            <br>
+            <input class="editingButton" type="submit" value="Make Changes">
+        </form>
+    <?php } ?>
+    <form action="about-delete-delete-pdo.php" method="POST">
+        <h1>Delete From Home</h1>
+        <h3>Delete Options</h3>
+        <select class="deleteAboutOptions" name="toDelete">
+            <?php
+            foreach ($aboutData as $deleteOption) {
+                echo "<option value='" . $deleteOption['id'] . "'>" . $deleteOption['id'] . ' - ' . $deleteOption['section_title'] . "</option>";
+            }
+            ?>
+        </select>
         <br>
             <input class="editingButton" type="submit" value="Delete">
     </form>
