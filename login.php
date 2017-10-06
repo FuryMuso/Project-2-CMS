9c6a7b634a4be341bb8a8531efabd82fbb4c0e4c
Dan Tucker <furymuso@aim.com>
2017-10-06 09:12:49 +0100

    amended foreach statment for about section

--- login.php
+++ login.php
@@ -10,21 +10,23 @@ if(isLoggedIn()) {
 <html lang = en>
 <body>
 <head>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,800" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Raleway: 400,700" rel="stylesheet">
+    <link href="login.css" type="text/css" rel="stylesheet">
     <title>CMS Login</title>
 </head>
 <h1>Login Page</h1>
 <form method="POST" action="../Project-2-CMS/home-form.php">
-    <div>
+    <div class="loginField">
         <h2>Username:</h2>
         <input type="text" name="Username">
     </div>
-    <div>
+    <div class="loginField">
         <h2>Password:</h2>
         <input type="password" name="Password">
     </div>
-    <div>
-        <p><br></p>
-        <input type="submit" value="Home">
+    <div class="loginButton">
+        <input type="submit" value="Acquiesce">
     </div>
 </form>
 </body>
